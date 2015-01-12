76421c98f86507fe50e48ea81e96248804435ebf
Johán Steven Arias <jsarias1993@gmail.com>
2015-01-12 16:50:40 -0500

    Testing Debug True

--- kbbs/settings.py
+++ kbbs/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '^ucnnzkve7%5wp_q6$hrnf-s!##1x5ih+l3#56__8&#plqaot5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = socket.gethostname() == 'Yohans-MacBook-Air.local'
+# DEBUG = socket.gethostname() == 'Yohans-MacBook-Air.local'
+
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
