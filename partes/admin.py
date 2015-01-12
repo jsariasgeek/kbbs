ed4d1bcd0ba30bd354f0558942086ca7c34718c4
Johán Steven Arias <jsarias1993@gmail.com>
2015-01-12 16:03:48 -0500

    Solver import Error Id

--- partes/admin.py
+++ partes/admin.py
@@ -6,6 +6,7 @@ from .models import Parte
 class ParteResource(resources.ModelResource):
 	class Meta():
 		model = Parte
+		import_id_fields = ['numero_de_parte']
 
 class ParteAdmin(ImportExportModelAdmin):
 	list_display = ('numero_de_parte', 'descripcion', 'dispositivo')
