d6eb0d8d026024ff074dfabfef54f866ff711983
Johán Steven Arias <jsarias1993@gmail.com>
2015-01-12 22:18:36 -0500

    Solved String

--- envios_kbbs/admin.py
+++ envios_kbbs/admin.py
@@ -4,7 +4,7 @@ from .models import Envio
 class EnvioAdmin(admin.ModelAdmin):
 	list_display = ('fecha', 'get_number_parte', 'parte', 'sro', 'fecha_envio', 'waybill', 'picture', 'responsable_dhl',)
 	raw_id_fields = ('parte',)
-	search_fields = ['parte__numero_de_parte', 'parte__descripcion']
+	search_fields = ['parte__numero_de_parte', 'parte__descripcion', 'sro',]
 
 
 admin.site.register(Envio, EnvioAdmin)
