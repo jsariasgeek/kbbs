e36cd76629282ade3f103833e1df2c7fa1f530aa
Johán Steven Arias <jsarias1993@gmail.com>
2015-01-13 21:57:51 -0500

    Sorted

--- envios_kbbs/admin.py
+++ envios_kbbs/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Envio
 
 class EnvioAdmin(admin.ModelAdmin):
-	list_display = ('parte', 'sro', 'fecha_envio', 'get_number_parte', 'waybill', 'show_thumbnail', 'responsable_dhl',)
+	list_display = ('sro', 'parte', 'fecha_envio', 'get_number_parte', 'waybill', 'show_thumbnail', 'responsable_dhl',)
 	raw_id_fields = ('parte',)
 	search_fields = ['parte__numero_de_parte', 'parte__descripcion', 'sro',]
 
