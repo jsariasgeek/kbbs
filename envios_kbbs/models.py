4104fdd8d8931279c64e6e0911e611f7a079cc4c
Johán Steven Arias <jsarias1993@gmail.com>
2015-01-13 23:35:03 -0500

    Solved Strings Modal 2

--- envios_kbbs/models.py
+++ envios_kbbs/models.py
@@ -25,7 +25,7 @@ class Envio(models.Model):
 		return self.parte.numero_de_parte
 
 	def show_thumbnail(self):		
-		return """<a><img src='%s' alt='%s'></a>""" %(self.avatar.url, self.sro, self.picture.url)
+		return """<a><img src='%s' alt='%s'></a>""" %(self.avatar.url, self.sro,)
 
 
 
